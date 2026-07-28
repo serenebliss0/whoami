ee11c8b26e6fff1bcfbfd547434b8a40c27368ba
Semire Ajayi <semajayi1234@gmail.com>
2026-07-28 02:19:39 +0100

    Build Deployment

--- whoami/build/web/flutter_bootstrap.js
+++ whoami/build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1952349932" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1979113892" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
